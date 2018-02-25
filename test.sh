b100e6b7d6f61e1f034e1d84664abdb9fdd66ca0
Xan Nick <xan.nick@gmail.com>
2018-02-25 00:58:39 -0600

    Use OpenSSL only, unique keys for each script, support named secrets, and use new home folder structure.

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-. ./encpass.sh
-password=$(get_password)
-# Call it specifying a directory
-#password=$(get_password ~/.ssh)
+. encpass.sh
+password=$(get_secret)
+# Call it specifying a named secret
+#password=$(get_secret password)
+# Call it specifying a named secret for a specific label
+#password=$(get_secret test.sh password)
 echo $password
