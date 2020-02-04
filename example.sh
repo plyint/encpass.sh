a1f17893686fc18bd2ce26ea797a5473add86fd6
Xan Nick <xan.nick@plyint.com>
2020-02-04 16:51:29 -0600

    Updated example.sh to make output of script clearer.

--- example.sh
+++ example.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-. encpass.sh
+. ./encpass.sh
 password=$(get_secret)
 # Call it specifying a named secret
 #password=$(get_secret password)
 # Call it specifying a named secret for a specific label
 #password=$(get_secret example.sh password)
-echo $password
+echo "password = $password"
